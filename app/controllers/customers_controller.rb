b454f64f60bd9eaee593abb4f54f3db2f98c3b3d
palmierie <ericmpalmieri@gmail.com>
2017-10-23 10:06:20 -0500

    save before merge

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,29 +1,29 @@
 class CustomersController < ApplicationController
 
-    def index
-        @customers = Customer.all
-        json_response(@customers)
-    end
+  def index
+      @customers = Customer.all
+      json_response(@customers)
+  end
 
-    def show
-        @customer = Customer.find(params[:id])
-        json_response(@customer)
-    end
+  def show
+      @customer = Customer.find(params[:id])
+      json_response(@customer)
+  end
 
-    def create
-        @customer = Customer.create(customer_params)
-        json_response(@customer)
-        # redirect_to @customer
-    end
+  def create
+      @customer = Customer.create(customer_params)
+      json_response(@customer)
+      # redirect_to @customer
+  end
 
-    def update
-        @customer = Customer.find(params[:id])
-        @customer.update(customer_params)
-        json_response(@customer)
+  def update
+      @customer = Customer.find(params[:id])
+      @customer.update(customer_params)
+      json_response(@customer)
+  end
+
+  private
+    def customer_params
+        params.require(:order).permit(:customer_first_name, :customer_last_name, :customer_acct_date, :customer_active)
     end
-       
-    private
-        def customer_params
-            params.require(:customer).permit(:customer_first_name, :customer_last_name, :customer_acct_date, :customer_active)
-        end
 end
---
+++ app/controllers/product_types_controller.rb
@@ -0,0 +1,29 @@
+class ProductTypesController < ApplicationController
+
+  def index
+      @product_types = ProductType.all
+      json_response(@product_types)
+  end
+
+  def show
+      @product_type= ProductType.find(params[:id])
+      json_response(@product_type)
+  end
+
+  def create
+      @product_type ProductType.create(product_type_params)
+      json_response(@product_type)
+      # redirect_to @product_type
+  end
+
+  def update
+      @product_type = ProductType.find(params[:id])
+      @product_type.update(product_type_params)
+      json_response(@product_type)
+  end
+
+  private
+    def product_type_params
+        params.require(:product_type).permit(:product_type_name)
+    end
+end
