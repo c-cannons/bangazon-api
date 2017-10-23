6bfaee51a4359f39b16ceea0c2eead7a92be3f0a
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-23 09:56:56 -0500

    Ran migration and tested http verbs on customer controller

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,34 @@
+class ProductController < ApplicationController
+
+  def index
+    @products = Product.all
+    json_response(@products)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    json_response(@product)
+  end
+
+  def create
+    @product = Customer.create(product_params)
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @customer.update(product_params)
+    json_response(product)
+  end
+
+  def delete
+    @product = Product.find(params[:id])
+    @product.destroy
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:product_name, :product_price, :product_description, :product_type_id )
+    end
+
+end
