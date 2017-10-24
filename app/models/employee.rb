57e27d3aeb4016344fc91b5aad25c87d59c15976
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-24 10:41:22 -0500

    join table relationships

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,8 @@
 class Employee < ApplicationRecord
-  has_one :computer
+  # has_one :computer
   belongs_to :department
-  has_many :training_classes, through: :training_seats
+  has_many :training_classes, through: :training_seat
+  has_many :computers, through: :employee_computer
 
   validates_presence_of :employee_first_name, :employee_last_name, :employee_title, :department_id
 end
--- db/migrate/20171020164522_create_training_seats.rb
+++ db/migrate/20171020164522_create_training_seats.rb
@@ -1,8 +1,8 @@
 class CreateTrainingSeats < ActiveRecord::Migration[5.1]
   def change
     create_table :training_seats do |t|
-      t.integer :class_id
-      t.integer :employee_id
+      t.references :class_id, null: false, foreign_key: true
+      t.references :employee_id, null: false, foreign_key: true
 
       t.timestamps
     end
