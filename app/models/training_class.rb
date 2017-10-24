c19a0b7e18b259c08077405da5d6aab749a0271b
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-24 09:36:27 -0500

    training class relationships

--- app/models/training_class.rb
+++ app/models/training_class.rb
@@ -1,2 +1,9 @@
 class TrainingClass < ApplicationRecord
+
+  has_many :employees, through: :training_seats
+
+  validates_presence_of :class_name, :start_date, :end_date, :max_seats
+
+  validates :max_seats, numericality: { less_than_or_equal_to: 20 }
+
 end
