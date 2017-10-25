d697a1272a0b30a8d9fcb0ab4933fbbead96bc15
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-25 10:00:39 -0500

    made payment type nullible

--- db/migrate/20171020164829_create_orders.rb
+++ db/migrate/20171020164829_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.references :customers, index: true, null: false, foreign_key: true
-      t.references :pay_methods, index: true, null: false, foreign_key: true
+      t.references :pay_methods, index: true, foreign_key: true
 
       t.timestamps
     end
