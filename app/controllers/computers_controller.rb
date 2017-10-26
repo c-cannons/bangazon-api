1b4c3d001eeac17df8b719e80b29459196d0cfda
Apple <Admin@Matt-Browns-iMac.local>
2017-10-26 09:22:15 -0500

    storing mistakes

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -1,32 +1,48 @@
 class ComputersController < ApplicationController
-
+# GET /computers
 def index
   @computers = Computer.all
-  json_response(@computers)
+  render json: @computers
 end
 
+# GET /computers/1
 def show
   @computer = Computer.find(params[:id])
-  json_response(@computer)
+  render json: @computer
 end
 
+# POST /computers
 def create
-  @computer = Computer.create(computer_params)
-  json_response(@computer)
+
+  @computer = Computer.new(computer_params)
+
+  if @computer.save
+    render json: @computer, status: :created, location: @computer
+  else
+    render json: @computer.errors, status: :unprocessable_entity
+  end
+
 end
 
+#PATCH/PUT /employees/1
 def update
-  @computer = Computer.find(params[:id])
-  @computer.update(computer_params)
-  json_response(@computer)
+
+  if @computer.update(computer_params)
+    render json: @computer
+  else
+    render json: @computer.errors, status: :unprocessable_entity
+  end
 end
 
 def destroy
-  @computer = Computer.find(params[:id])
   @computer.destroy
 end
 
 private
+
+def set_employee
+  @employee = Employee.find(params[:id])
+end
 
 def computer_params
   params.require(:computers).permit(:computer_brand, :computer_model, :purchase_date, :decomm_date)
