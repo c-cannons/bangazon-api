193cb2062ec8e12ea0d90594f001955385efc419
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-10-26 10:22:22 -0500

    updated orders controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@
 #     product_name: Faker::Pokemon.name,
 #     product_price: Faker::Number.decimal(2),
 #     product_description: Faker::RickAndMorty.quote,
-#     product_types_id: Faker::Number.number(1..5)
+#     product_types_id: rand(1..5)
 #   }])
 # end
 
@@ -38,12 +38,12 @@
   #   account_number: Faker::Business.credit_card_number
   # }])
 
-# 10.times do
-#   Order.create([{
-#     customers_id: Faker::Number.number(2),
-#     pay_methods_id: Faker::Number.number(2)
-#   }])
-# end
+10.times do
+  Order.create([{
+    customers_id: Faker::Number.number(2),
+    pay_methods_id: Faker::Number.number(2)
+  }])
+end
 
 # 10.times do
 #   ProductType.create([{
@@ -83,11 +83,11 @@
 #   }])
 # end
 
-10.times do
-  OrderDetail.create([{
-    products_id: rand(1..10),
-    orders_id: rand(1..10),
-    discount: Faker::Number.decimal(1),
-    ext_price: Faker::Number.decimal(2)
-  }])
-end
+# 10.times do
+#   OrderDetail.create([{
+#     products_id: rand(1..10),
+#     orders_id: rand(1..10),
+#     discount: Faker::Number.decimal(1),
+#     ext_price: Faker::Number.decimal(2)
+#   }])
+# end
